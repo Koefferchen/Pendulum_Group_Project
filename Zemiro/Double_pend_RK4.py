65a3497ae1bd3f262383716eb92240962d1d1158
zem047 <zem04@hotmail.co.uk>
2025-01-23 13:43:10 +0000

    Updated version

--- Zemiro/Double_pend_RK4.py
+++ Zemiro/Double_pend_RK4.py
@@ -38,30 +38,27 @@ def RK4_double_pendulum(t1, theta1_0, theta2_0, theta1_dot_0, theta2_dot_0, L1, 
         """
         theta1, theta2, theta1_dot, theta2_dot = y
 
-        delta = theta2 - theta1
-        denom1 = L1 * (2 * m1 + m2 - m2 * np.cos(2 * delta))
-        denom2 = L2 * (2 * m1 + m2 - m2 * np.cos(2 * delta))
+        mass_diff=m2/(m1+m2)
+        delta = theta1 - theta2
+        denom1 = (1-((np.cos(delta)*np.cos(delta)) * mass_diff))
+        denom2 = (1-((np.cos(delta)*np.cos(delta)) * mass_diff))
 
         theta1_ddot = (
-            -g * (2 * m1 + m2) * np.sin(theta1)
-            - m2 * g * np.sin(theta1 - 2 * theta2)
-            - 2 * np.sin(delta) * m2 * (theta2_dot**2 * L2 + theta1_dot**2 * L1 * np.cos(delta))
+            g/L1 *( (mass_diff * np.sin(theta2) * np.cos(delta) ) - np.sin(theta1) )
+            - (np.sin(delta) * mass_diff * (theta2_dot*theta2_dot * (L2/L1) + theta1_dot*theta1_dot * np.cos(delta)))
         ) / denom1
 
         theta2_ddot = (
-            2 * np.sin(delta)
-            * (
-                theta1_dot**2 * L1 * (m1 + m2)
-                + g * (m1 + m2) * np.cos(theta1)
-                + theta2_dot**2 * L2 * m2 * np.cos(delta)
-            )
-        ) / denom2
+                g/L2 *( (mass_diff * np.sin(theta1) * np.cos(delta)) - np.sin(theta2) )
+            + np.sin(delta) * (theta1_dot*theta1_dot * (L1/L2) + theta2_dot*theta2_dot * mass_diff * np.cos(delta))
+       
+            ) / denom2
 
         return np.array([theta1_dot, theta2_dot, theta1_ddot, theta2_ddot])
 
     # Initialize arrays
-    t = np.arange(0, t1 + h, h)
-    n = len(t)
+    t = np.arange(0,t1+h,h)
+    n = np.shape(t)[0]
     theta1 = np.zeros(n)
     theta2 = np.zeros(n)
     theta1_dot = np.zeros(n)
@@ -86,16 +83,19 @@ def RK4_double_pendulum(t1, theta1_0, theta2_0, theta1_dot_0, theta2_dot_0, L1, 
 
         theta1[i], theta2[i], theta1_dot[i], theta2_dot[i] = y_next
 
+    theta1 = np.mod(theta1+np.pi, 2*np.pi)-np.pi
+    theta2 = np.mod(theta2+np.pi, 2*np.pi)-np.pi
+    
     return t, theta1, theta2, theta1_dot, theta2_dot
 
 # Parameters
 L1, L2 = 1.0, 1.0  # Lengths of the pendulums
 m1, m2 = 1.0, 1.0  # Masses of the pendulum bobs
 g =sp.constants.g         # Gravitational acceleration
-theta1_0, theta2_0 = np.pi / 4, np.pi / 6  # Initial angles
-theta1_dot_0, theta2_dot_0 = 0, 0         # Initial angular velocities
-t1 = 20.0          # Simulation time
-h = 0.01           # Time step
+theta1_0, theta2_0 = 1*np.pi, 0.01*np.pi  # Initial angles
+theta1_dot_0, theta2_dot_0 = 0.0, 0.0         # Initial angular velocities
+t1 = 10.0          # Simulation time
+h = 0.001           # Time step
 
 # Simulate
 t, theta1, theta2, theta1_dot, theta2_dot = RK4_double_pendulum(
