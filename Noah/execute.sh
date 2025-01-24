f87b147db881dbfe87435568323b4b0b3d4a924f
Koefferchen <hostedbysome@gmail.com>
2025-01-24 18:23:52 +0000

    colorbar done, want poincare animation

--- Noah/execute.sh
+++ Noah/execute.sh
@@ -6,7 +6,7 @@ make
 cd ../Python-Code
 
 #python3 plotting_simp_pend.py
-python3 plotting_doub_pend.py
+#python3 plotting_doub_pend.py
 #python3 plotting_trip_pend.py
 #python3 animating_doub_pend.py
 #python3 animating_trip_pend.py
